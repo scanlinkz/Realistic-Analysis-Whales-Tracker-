39050f938d228aaa8e5eb3c3803c18ce7746db3b
scanlinkz <scanlinkz@gmail.com>
2026-05-23 13:35:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import threading
+import os
+
 app = Flask(__name__)
+
 @app.route('/')
-def home(): return "Bot is running"
+def home():
+    return "Bot is running"
+
+def run_bot():
+    os.system("python bot.py")
+
 if __name__ == '__main__':
-    import threading
-    def run_bot():
-        import os
-        os.system("python bot.py")
     threading.Thread(target=run_bot).start()
     app.run(host='0.0.0.0', port=10000)
